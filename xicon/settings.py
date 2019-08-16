baf73a7dbf0dd0a2b81b306315bd845872fdefa3
Alexei Andrushievich <vint21h@vint21h.pp.ua>
2019-08-17 01:51:48 +0300

    Add xicon_apple_touch_icon and xicon_apple_touch_icons templatetags, some self stupidity fixes

--- xicon/settings.py
+++ xicon/settings.py
@@ -9,9 +9,27 @@ from typing import Dict, Union, Iterable
 from django.conf import settings
 
 
-__all__ = ["FAVICONS"]  # type: list
+__all__ = [
+    "FAVICONS",
+    "APPLE_TOUCH_ICONS",
+    "APPLE_TOUCH_ICON_MASK_COLOR",
+    "APPLE_MOBILE_WEB_APP_STATUS_BAR_STYLE_COLOR",
+    "APPLE_MOBILE_WEB_APP_TITLE",
+]  # type: list
 
 
 FAVICONS = getattr(
     settings, "XICON_FAVICONS", []
 )  # type: Iterable[Dict[str, Union[str, Dict[str, int], None]]]
+APPLE_TOUCH_ICONS = getattr(
+    settings, "XICON_APPLE_TOUCH_ICONS", []
+)  # type: Iterable[Dict[str, Union[str, Dict[str, int], None]]]
+APPLE_TOUCH_ICON_MASK_COLOR = getattr(
+    settings, "XICON_APPLE_TOUCH_ICON_MASK_COLOR", ""
+)  # type: str
+APPLE_MOBILE_WEB_APP_STATUS_BAR_STYLE_COLOR = getattr(
+    settings, "XICON_APPLE_MOBILE_WEB_APP_STATUS_BAR_STYLE_COLOR", ""
+)  # type: str
+APPLE_MOBILE_WEB_APP_TITLE = getattr(
+    settings, "XICON_APPLE_MOBILE_WEB_APP_TITLE", ""
+)  # type: str
--- xicon/templatetags/xicon_tags.py
+++ xicon/templatetags/xicon_tags.py
@@ -4,14 +4,19 @@
 # xicon/templatetags/xicon_tags.py
 
 
-from typing import Dict, Union, Iterable
+from typing import Dict, Union
 
 from django import template
 
-from xicon.settings import FAVICONS
+from xicon.settings import FAVICONS, APPLE_TOUCH_ICONS
 
 
-__all__ = ["xicon_favicon"]  # type: list
+__all__ = [
+    "xicon_favicon",
+    "xicon_favicons",
+    "xicon_apple_touch_icon",
+    "xicon_apple_touch_icons",
+]  # type: list
 
 
 register = template.Library()
@@ -19,8 +24,7 @@ register = template.Library()
 
 @register.inclusion_tag("xicon/templatetags/xicon_favicon.html", takes_context=True)
 def xicon_favicon(
-    context: template.Context,
-    favicon: Iterable[Dict[str, Union[str, Dict[str, int], None]]],
+    context: template.Context, favicon: Dict[str, Union[str, Dict[str, int], None]]
 ) -> template.Context:
     """
     Render classic favicon meta tag.
@@ -39,7 +43,7 @@ def xicon_favicon(
 
 
 @register.inclusion_tag("xicon/templatetags/xicon_favicons.html", takes_context=True)
-def xicon_favicons(context: template.Context,) -> template.Context:
+def xicon_favicons(context: template.Context) -> template.Context:
     """
     Render classic favicon meta tags.
 
@@ -50,5 +54,46 @@ def xicon_favicons(context: template.Context,) -> template.Context:
     """
 
     context.update({"XICON_FAVICONS": FAVICONS})
+
+    return context
+
+
+@register.inclusion_tag(
+    "xicon/templatetags/xicon_apple_touch_icon.html", takes_context=True
+)
+def xicon_apple_touch_icon(
+    context: template.Context,
+    apple_touch_icon: Dict[str, Union[str, Dict[str, int], None]],
+) -> template.Context:
+    """
+    Render apple touch icon meta tag.
+
+    :param context: template context.
+    :type context: django.template.Context.
+    :param apple_touch_icon: dict containing favicon settings.
+    :type apple_touch_icon: Iterable[Dict[str, Union[str, Dict[str, int], None]]].
+    :return: updated template context.
+    :rtype: django.template.Context.
+    """
+
+    context.update({"XICON_APPLE_TOUCH_ICON": apple_touch_icon})
+
+    return context
+
+
+@register.inclusion_tag(
+    "xicon/templatetags/xicon_apple_touch_icons.html", takes_context=True
+)
+def xicon_apple_touch_icons(context: template.Context) -> template.Context:
+    """
+    Render apple touch icon meta tags.
+
+    :param context: template context.
+    :type context: django.template.Context.
+    :return: updated template context.
+    :rtype: django.template.Context.
+    """
+
+    context.update({"XICON_APPLE_TOUCH_ICONS": APPLE_TOUCH_ICONS})
 
     return context
