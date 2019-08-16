b946ef73dff3e35cda707f86332131c16be7de14
Alexei Andrushievich <vint21h@vint21h.pp.ua>
2019-08-17 01:20:33 +0300

    Add xicon_favicon templatetag

--- xicon/settings.py
+++ xicon/settings.py
@@ -4,4 +4,14 @@
 # xicon/settings.py
 
 
+from typing import Dict, Union, Iterable
+
+from django.conf import settings
+
+
 __all__ = []  # type: list
+
+
+FAVICONS = getattr(
+    settings, "XICON_FAVICONS", []
+)  # type: Iterable[Dict[str, Union[str, Dict[str, int], None]]]
--- xicon/templatetags/xicon_tags.py
+++ xicon/templatetags/xicon_tags.py
@@ -4,4 +4,33 @@
 # xicon/templatetags/xicon_tags.py
 
 
-__all__ = []  # type: list
+from typing import Dict, Union, Iterable
+
+from django import template
+
+
+__all__ = ["xicon_favicon"]  # type: list
+
+
+register = template.Library()
+
+
+@register.inclusion_tag("xicon/templatetags/xicon_favicon.html", takes_context=True)
+def xicon_favicon(
+    context: template.Context,
+    favicon: Iterable[Dict[str, Union[str, Dict[str, int], None]]],
+) -> template.Context:
+    """
+    Render classic favicon meta tag.
+
+    :param context: template context.
+    :type context: django.template.Context.
+    :param favicon: dict containing favicon settings.
+    :type favicon: Iterable[Dict[str, Union[str, Dict[str, int], None]]].
+    :return: updated template context.
+    :rtype: django.template.Context.
+    """
+
+    context.update({"XICON_FAVICON": favicon})
+
+    return context
