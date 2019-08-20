882edba14d80705eee1c0bf8758fdeaf2fb7ec48
Alexei Andrushievich <vint21h@vint21h.pp.ua>
2019-08-21 01:08:34 +0300

    Add 'xicon_favicons' templatetag tests

--- xicon/tests/templatetags/test_xicon_tags.py
+++ xicon/tests/templatetags/test_xicon_tags.py
@@ -7,10 +7,10 @@
 from django.template import Context, Template
 from django.test import TestCase
 
-from xicon.templatetags.xicon_tags import xicon_favicon
+from xicon.templatetags.xicon_tags import xicon_favicon, xicon_favicons
 
 
-__all__ = ["XiconFaviconTest"]  # type: list
+__all__ = ["XiconFaviconTest", "XiconFaviconsTest"]  # type: list
 
 
 class XiconFaviconTest(TestCase):
@@ -52,10 +52,10 @@ class XiconFaviconTest(TestCase):
         }  # type: dict
         context = Context({"XICON_FAVICON": favicon})
         template = Template("{% load xicon_tags %}" "{% xicon_favicon XICON_FAVICON %}")
-        rendered = template.render(context)  # type: str
-        result = '<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" sizes="16x16"/>'  # type: str
+        response = template.render(context)  # type: str
+        expected = '<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" sizes="16x16"/>'  # type: str
 
-        self.assertInHTML(needle=result, haystack=rendered)
+        self.assertInHTML(needle=expected, haystack=response)
 
     def test_xicon_favicon__render__without_size(self) -> None:
         """
@@ -68,7 +68,44 @@ class XiconFaviconTest(TestCase):
         favicon = {"src": "favicon.svg", "type": "image/svg+xml"}  # type: dict
         context = Context({"XICON_FAVICON": favicon})
         template = Template("{% load xicon_tags %}" "{% xicon_favicon XICON_FAVICON %}")
-        rendered = template.render(context)  # type: str
-        result = '<link rel="shortcut icon" href="favicon.svg" type="image/svg+xml" sizes="any"/>'  # type: str
+        response = template.render(context)  # type: str
+        expected = '<link rel="shortcut icon" href="favicon.svg" type="image/svg+xml" sizes="any"/>'  # type: str
 
-        self.assertInHTML(needle=result, haystack=rendered)
+        self.assertInHTML(needle=expected, haystack=response)
+
+
+class XiconFaviconsTest(TestCase):
+    """
+    Favicons templatetag tests.
+    """
+
+    def test_xicon_favicons__return_context(self) -> None:
+        """
+        Test templatetag returning context.
+
+        :return: nothing.
+        :rtype: None.
+        """
+
+        context = Context()
+
+        self.assertIsInstance(obj=xicon_favicons(context=context), cls=Context)
+
+    def test_xicon_favicons__render(self) -> None:
+        """
+        Test templatetag rendering result.
+
+        :return: nothing.
+        :rtype: None.
+        """
+
+        context = Context()
+        template = Template("{% load xicon_tags %}" "{% xicon_favicons %}")
+        response = template.render(context)  # type: str
+        expected = """
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" sizes="16x16"/>
+        <link rel="shortcut icon" href="favicon.png" type="image/png" sizes="32x32"/>
+        <link rel="shortcut icon" href="favicon.svg" type="image/svg+xml" sizes="any"/>
+        """  # type: str
+
+        self.assertInHTML(needle=expected, haystack=response)
