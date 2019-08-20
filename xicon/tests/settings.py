2394963659d5dc1034b18f51517e0f4a4ca21fc9
Alexei Andrushievich <vint21h@vint21h.pp.ua>
2019-08-21 00:08:09 +0300

    Update test settings

--- xicon/tests/settings.py
+++ xicon/tests/settings.py
@@ -5,13 +5,13 @@
 
 
 # secret key minimum enough config for django
-SECRET_KEY = "xicon-test-key"
+SECRET_KEY = "xicon-test-key"  # type: str
 
 # add nose test runner application and django-xicon
-INSTALLED_APPS = ["django_nose", "xicon"]
+INSTALLED_APPS = ["django_nose", "xicon"]  # type: list
 
 # add nose test runner
-TEST_RUNNER = "django_nose.NoseTestSuiteRunner"
+TEST_RUNNER = "django_nose.NoseTestSuiteRunner"  # type: str
 
 # configure nose test runner
 NOSE_ARGS = [
@@ -24,7 +24,7 @@ NOSE_ARGS = [
     "--cover-erase",
     "--cover-package=xicon",
     "--logging-clear-handlers",
-]
+]  # type: list
 
 # configure urls
-ROOT_URLCONF = __name__
+ROOT_URLCONF = __name__  # type: str
